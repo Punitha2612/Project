71915f2a5769c45da0b87ca1aaed7810f34fc39c
Punitha <punitha2612@gmail.com>
2025-01-05 12:01:27 +0530

    5th jan

---
+++ src/Lab_02.java
@@ -0,0 +1,8 @@
+public class Lab_02 {
+    public static void main(String args[]) {
+
+        System.out.println("Hellooooooo");
+        System.out.print("Hellooooooo");//printing 0 and 1
+
+    }
+}
